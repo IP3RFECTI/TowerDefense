370680f1226e79b985dc29b87387523530a68b72
GitAcc011 <57559428+GitAcc011@users.noreply.github.com>
2024-06-01 21:31:52 +0300

    catapult.py animation

--- catapult.py
+++ catapult.py
@@ -5,52 +5,39 @@ from random import randrange
 
 class Catapult(pygame.sprite.Sprite):
     """enemy actions"""
+
     def __init__(self, screen, spawn_point):
         """start position"""
         super(Catapult, self).__init__()
         self.screen = screen
+        self.height = self.screen.get_height() * 0.6
         # self.direction = direction
         self.spawn_point = spawn_point
-        self.tower_image = pygame.transform.flip(pygame.image.load('assets/animations/katapulthrowing/1.png'), 1, 0)
-        self.screen.blit(self.tower_image, (spawn_point, 0))
+        self.end_point = spawn_point / 10 * 9
+        self.catapult_image = pygame.transform.flip(pygame.image.load('assets/animations/katapulthrowing/1.png'), 1, 0)
         """animation"""
         self.frame_images = []
         for i in range(1, 9):
-            self.frame_images.append(pygame.transform.flip(pygame.image.load(f"assets/animations/katapulthrowing/{i}.png"), 1, 0))
+            self.frame_images.append(
+                pygame.transform.flip(pygame.image.load(f"assets/animations/katapulthrowing/{i}.png"), 1, 0))
+        self.counter = 0
         self.animation_length = len(self.frame_images)
-        self.animation_speed = 15
-        self.current_frame_index = 0
-        self.animation_timer = 0
-        self.frame_position = [0, 0]
-        self.window_height = self.screen.get_height()
-        self.frame_height = self.frame_images[0].get_height()
-        self.frame_position[1] = int(self.window_height * 0.45) - int(self.frame_height / 2)
-        self.clock = pygame.time.Clock()
-        """moving"""
-        self.rect = self.tower_image.get_rect()
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-        self.speed = 1
-        self.x = float(self.rect.x)
-        self.y = float(self.rect.y)
 
+        self.clock = pygame.time.Clock()
 
     def draw_catapult(self):
-        """show catapult"""
-        self.screen.blit(self.tower_image, (self.spawn_point, self.screen.get_height()*0.6))
-
+        """catapult rides"""
+        if self.spawn_point >= self.end_point:
+            self.screen.blit(self.catapult_image, (self.spawn_point, self.height))
+            self.spawn_point -= 1
+        else:
+            self.play_animation()
 
     def play_animation(self):
-        """catapult rides"""
-        # обновление состояния
-        time_delta = self.clock.tick(60) / 1000.0
-        self.animation_timer += time_delta
-        if self.animation_timer >= 1.0 / self.animation_speed:
-            current_frame_index = (self.current_frame_index + 1) % self.animation_length
-            self.animation_timer -= 1.0 / self.animation_speed
-
-        self.frame_position[0] -= 1  # сдвигаем кадр влево
-        # выводим кадры, обновляем экран
-        current_frame = self.frame_images[self.current_frame_index]
-        self.screen.blit(current_frame, self.frame_position)
+        """catapult throws"""
+        self.counter += 1
+        if (self.counter >= self.animation_length):
+            self.counter = 1
+        self.screen.blit(self.frame_images[self.counter], (self.spawn_point, self.height))
         pygame.display.flip()
+        self.clock.tick(8)
